eed0946c35af0bef3d6494f0a970ca573037d92c
Jo1ner <semdimas@gmail.com>
2023-08-19 21:02:03 +0300

    add task-4

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const value = document.querySelector('#value');
+let counterValue = 0;
+
+
+const decrementValue = document.querySelector('[data-action="decrement"]');
+const incrementValue = document.querySelector('[data-action="increment"]');
+
+decrementValue.addEventListener("click", plusOne);
+function plusOne() {
+    counterValue += 1;
+    value.textContent = counterValue;
+};
+
+incrementValue.addEventListener("click", minusOne);
+function minusOne() {
+    counterValue -= 1;
+    value.textContent = counterValue;
+};
