7dc6284bd8b597253ac041d3595641691eba3aa5
Jo1ner <semdimas@gmail.com>
2023-08-19 11:46:00 +0300

    add task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const array = [];
+ingredients.forEach(ingredient => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingredient;
+  liEl.classList.add("item");
+  array.push(liEl);
+  
+});
+console.log(array);
+ingredientsList.append(...array);
+console.log(ingredientsList);
+
