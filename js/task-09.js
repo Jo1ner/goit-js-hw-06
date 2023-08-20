9efb01a8aed0545572f4f52285c486e8a2ae3a3a
Jo1ner <semdimas@gmail.com>
2023-08-20 12:03:04 +0300

    add task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const span = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+
+btn.addEventListener('click', changeColor);
+function changeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
