65cadecd5994756e31195e7700978d4c177da8cf
Jo1ner <semdimas@gmail.com>
2023-08-20 11:14:49 +0300

    add task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+    if (email.value === "" || password.value === "") {
+        alert("Всі поля повинні бути заповнені");
+    }
+    else {
+        const data = {
+        email: email.value,
+        password: password.value,
+    }
+    console.dir(data);
+    formEl.reset();
+    }
+}
